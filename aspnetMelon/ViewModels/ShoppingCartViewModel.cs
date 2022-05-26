681056bdac9c73a3a2cb389ebf894c99245d4d9c
Kyemate <henke@mdpn.net>
2022-05-26 23:10:36 +0200

    yehaaa

---
+++ aspnetMelon/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Models;
+
+namespace aspnetMelon.ViewModels;
+
+internal class ShoppingCartViewModel
+{
+    public ShoppingCart ShoppingCart { get; set; } = default!;
+    public decimal ShoppingCartTotal { get; set; }
+}
