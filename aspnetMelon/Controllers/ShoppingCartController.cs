d6a471b1146dac5c09fa8e2c91010f227737c1ca
alexmar000 <alex.olave@inteheltfel.se>
2022-05-26 20:04:56 +0200

    l

--- aspnetMelon/Controllers/ShoppingCartController.cs
+++ aspnetMelon/Controllers/ShoppingCartController.cs
@@ -5,15 +5,56 @@ namespace aspnetMelon.Controllers
     public class ShoppingCartController : Controller
     {
         private readonly IProductService _productService;
+        private readonly IShoppingCartService _shoppingCartService;
 
-        public ShoppingCartController(IProductService productService)
+        public ShoppingCartController(IProductService productService, IShoppingCartService shoppingCartService)
         {
             _productService = productService;
+            _shoppingCartService = shoppingCartService;
         }
 
-        //public IActionResult AddToShoppingCart()
-        //{
+        public ViewResult Index()
+        {
+            _shoppingCartService.ShoppingCartItems = _shoppingCart.GetShoppingCartItems();
 
-        //}
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(shoppingCartViewModel);
+        }
+
+        public RedirectToActionResult AddToShoppingCart(int candyId)
+        {
+            var selectedCandy = _candyRepositoyr.GetAllCandy.FirstOrDefault(c => c.CandyId == candyId);
+
+            if (selectedCandy != null)
+            {
+                _shoppingCart.AddToCart(selectedCandy, 1);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public RedirectToActionResult RemoveFromShoppingCar(int candyId)
+        {
+            var selectedCandy = _candyRepositoyr.GetAllCandy.FirstOrDefault(c => c.CandyId == candyId);
+
+            if (selectedCandy != null)
+            {
+                _shoppingCart.RemoveFromCart(selectedCandy);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public RedirectToActionResult clearCart()
+        {
+            _shoppingCart.ClearCart();
+            return RedirectToAction("Index");
+        }
     }
+}
 }
