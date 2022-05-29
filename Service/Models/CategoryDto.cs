c45514a5119ad15c31e0c0499da349b56f63ecff
Kyemate <henke@mdpn.net>
2022-05-29 15:52:15 +0200

    fixi fix

--- Service/Models/CategoryDto.cs
+++ Service/Models/CategoryDto.cs
@@ -6,5 +6,5 @@ public record CategoryDto(int CategoryId, string CategoryName, string CategoryDe
 {
     
     public static implicit operator CategoryDto(Category category) =>
-        new (category.CategoryId,category.CategoryName,category.CategoryDescription, category.Products?.Select(p => (ProductDto) p).ToList());
+        new (category.CategoryId,category.CategoryName,category.CategoryDescription, category.Products?.Select(p => (ProductDto) p!).ToList());
 }
