19819918f08ccac9e85091c96d913f701e734ca7
alexmar000 <alex.olave@inteheltfel.se>
2022-05-31 22:10:23 +0200

    ss

--- Service/Models/OrderDetailDto.cs
+++ Service/Models/OrderDetailDto.cs
@@ -1,9 +1,9 @@
 ﻿namespace Service.Models;
 
-public record OrderDetailDto(int OrderDetailId, int OrderId, int ProductId, int Amount, decimal Price)
+public record OrderDetailDto(int OrderDetailId, int OrderId, int ProductId, int Amount, decimal Price, ProductDto? product = null)
 {
     public static implicit operator OrderDetailDto(OrderDetail orderDetail) =>
-        new(orderDetail.OrderDetailId, orderDetail.OrderId, orderDetail.ProductId, orderDetail.Amount, orderDetail.Price);
+        new(orderDetail.OrderDetailId, orderDetail.OrderId, orderDetail.ProductId, orderDetail.Amount, orderDetail.Price, orderDetail.Product);
 
     public static implicit operator OrderDetail(OrderDetailDto orderDetail) =>
     new(orderDetail.OrderId, orderDetail.ProductId, orderDetail.Amount, orderDetail.Price);
