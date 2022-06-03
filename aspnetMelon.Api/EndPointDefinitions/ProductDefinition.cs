4641993c0b8bffe73657678480fb7d1cf9012266
Kyemate <henke@mdpn.net>
2022-06-03 15:32:06 +0200

    Migration ApiKey

--- aspnetMelon.Api/EndPointDefinitions/ProductDefinition.cs
+++ aspnetMelon.Api/EndPointDefinitions/ProductDefinition.cs
@@ -4,7 +4,14 @@ public class ProductDefinition : IEndpointDefinition
 {
     public void DefineEndpoints(WebApplication app)
     {
-        app.MapGet("/test/",() => "test");
+        app.MapGet("/test/", (int personId) =>
+        {
+
+            return "test";
+        });
+
+     
+
     }
 
     public void DefineServices(IServiceCollection services)
